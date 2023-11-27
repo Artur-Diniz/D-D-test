f0b826514231f3ac0f7593e199b8fc471d7abebc
artur <arturdiniz06@gmail.com>
2023-11-27 20:51:12 -0300

    Inicio projeto Consumindo APi de DnD

---
+++ dandd/Models/Raca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace dandd.Models
+{
+    public class Raca
+    {
+    
+            
+        public string name { get; set; }
+        public string index { get; set; }
+        public string url { get; set; }
+    }
+}
